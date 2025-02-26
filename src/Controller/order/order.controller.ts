3e821638ae2a0369e26a2388fa623588ce5a3b22
BlackKey <553180854@qq.com>
2025-02-26 15:54:04 +0800

    完成订单

--- src/Controller/order/order.controller.ts
+++ src/Controller/order/order.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { OrderService } from './order.service';
+import { CreateOrderDto } from './order.dto';
 
 @Controller('/order')
 export class OrderController {
   constructor(private readonly service: OrderService) {}
+
+  @Get('/list')
+  async getOrderList(
+    @Query('page', new DefaultValuePipe(1)) page: number,
+    @Query('pageSize', new DefaultValuePipe(10)) pageSize: number,
+  ) {
+    return this.service.getOrderList({ page, pageSize });
+  }
+
+  @Post('/operate')
+  async operate(@Body() body: CreateOrderDto) {
+    return this.service.operate(body);
+  }
 }
--- src/Controller/order/order.service.ts
+++ src/Controller/order/order.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Order } from './order.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateOrderDto } from './order.dto';
+import { generateOrderId } from 'src/utils';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    @InjectRepository(Order)
+    private orderRepository: Repository<Order>,
+  ) {}
+  async getOrderList(params: {
+    page: number;
+    pageSize: number;
+  }): Promise<{ data: Order[]; total: number }> {
+    const { page, pageSize } = params;
+    const skip = (page - 1) * pageSize;
+    const [data, total] = await this.orderRepository.findAndCount({
+      where: { deleted: 0 },
+      select: [
+        'id',
+        'orderId',
+        'createTime',
+        'operatorId',
+        'operatorName',
+        'price',
+        'quantity',
+        'type',
+        'deleted',
+      ],
+      skip,
+      take: pageSize,
+    });
+    return { data, total };
+  }
+  operate(body: CreateOrderDto) {
+    const newOrder = this.orderRepository.create(body);
+    newOrder.orderId = generateOrderId();
+    this.orderRepository.save(newOrder);
+  }
+}
